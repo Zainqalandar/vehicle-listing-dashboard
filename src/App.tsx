3fa604a3141130bf16f59fe0a1de8ceae9f69632
syed-zain-stepsharp <syed.zain@stepsharp.dev>
2026-04-13 07:23:09 +0500

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        {/* Hello WOrld */}
+
+        <h1>Vehicle Listing Dashboard</h1>
+
+        
+      </section>
+    </>
+  )
+}
+
+export default App
